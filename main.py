72770041cba7a56051c21cd98b22be5dacf24af9
jax <102720664+jaxthechampion@users.noreply.github.com>
2022-07-09 02:53:10 +0000

    a

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from discord.ext import commands
 import os
 import discord
+intents = discord.Intents.default()
+intents.members = True
 
-command_prefix = '<'
-bot = commands.Bot(command_prefix = command_prefix)
+command_prefix = '>>'
+bot = commands.Bot(command_prefix = command_prefix, intents=intents)
+
+guild = bot.get_guild("919793000777908335")
 @bot.event
 async def on_ready():
-    print('son gay')
+    print('bot da san sang')
 #thêm process_commands 
 @bot.event
 async def on_message(message):
@@ -29,7 +33,36 @@ async def noigidi(ctx):
   
 @bot.command()
 async def hungdepzai(ctx):
-  await ctx.send(file=discord.File("hungngau.jpg"))
+  await ctx.send(file=discord.File("./files/hungngau.jpg"))
+@bot.command()
+async def vanmau(ctx):
+  await ctx.send(file=discord.File("./files/van_tan_gai.rtf"))
+@bot.command()
+async def mypfp(ctx):
+  await ctx.send(file=discord.File("./files/mypfp.png"))
+@bot.command()
+async def bvhocvahanh(ctx):
+  await ctx.send(file=discord.File("./files/BAI_VAN_HOC_VA_HANH.docx"))
+@bot.command()
+async def bvtuoitredatnuoc(ctx):
+  await ctx.send(file=discord.File("./files/BAI_VAN_TUOI_TRE_VA_TUONG_LAI_AT_NUOC.docx"))
+@bot.command(name='list')
+async def _list(ctx, arg):
+    pass
 
-'''import thư viện os để dùng biến môi trường (nếu up lên repo github mà không để token riêng thì sẽ bị reset)'''
+@commands.command()
+async def upf(ctx, *stuff):
+  msg  = ctx.message
+  if str(msg.attachments) == "[]":
+    await ctx.send("Bạn chưa gửi file nào!")
+  else:
+    split_v1 = str(msg.attachments).split("filename='")[1]
+    filename = str(split_v1).split("' ")[0]
+    await msg.attachments[0].save(fp="./files/{}".format(filename))
+
+@bot.command()
+async def somem(ctx):
+  await ctx.send(guild.member)
+
+'''import thư viện os để dùng biến môi trường (nếu up lên repo github mà không để token riêng thì sẽ bị reseta)'''
 bot.run(os.getenv('bot_token'))
